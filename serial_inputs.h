5d365e41b88ced0b47efbefe7f157fbcbda25727
Alvinosaur <ashek@andrew.cmu.edu>
2018-12-18 18:43:16 -0800

    Added initial scripts and headers for raspberry pi to arduino communication Added intial script for arduino motor controls for arm

---
+++ serial_inputs.h
@@ -0,0 +1,18 @@
+#ifndef _SERIAL_INPUTS_H_
+#define _SERIAL_INPUTS_H_
+
+#include "arm_data_struct.h"
+
+/* read serial input from raspberry pi:
+ *  end-effector goal position: (x, y)
+ *  end-effector goal velocity: (v_x, v_y)
+ * overall parse a string of floats separated by commas: x, y, v_x, v_y
+ */
+rec_arm_data *read_arm_goals();
+
+/* read initial arm pose for pre-game calibration
+ * v_x and v_y are dummy values
+ */
+regame_arm_data *read_arm_pregame();
+
+#endif
