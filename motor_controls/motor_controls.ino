805c89f67ee7604494c7e3c8e32dac5aee564595
Alvinosaur <ashek@andrew.cmu.edu>
2019-01-07 01:30:16 -0500

    added temporary files, just saving working

--- motor_controls/motor_controls.ino
+++ motor_controls/motor_controls.ino
@@ -3,82 +3,83 @@
 
 #include "serial_inputs.h"
 #include "arm_data_struct.h"
+#include "AccelStepper.h"
+#include "MultiStepper.h"
 
 #define LENGTH (double)5  // inches
 #define SQ(x) (double)(x*x)
 #define DEG_TO_RAD(x) ((double)x*PI/180)
 #define STEP_ROT DEG_TO_RAD(3)  // 3 degrees
+// TODO: Need to check number of steps for full rotation
+// one example uses 1350-ish, but other uses 200
+#define DEG_TO_STEP(x) ((x == 0) 0 : ((float)200/360)*(float)x)
+
+// define motor pins
+#define ARM0_M0_STEP 2
+#define ARM0_M0_DIR 3
+#define ARM0_M0_STEP 2
+#define ARM0_M0_DIR 3
+#define ARM0_M0_STEP 2
+#define ARM0_M0_DIR 3
+#define ARM0_M0_STEP 2
+#define ARM0_M0_DIR 3
 
 // arm0(left) and arm1(right) setup
-store_arm_data arm0;
-store_arm_data arm1;
+AccelStepper arm0_m0(AccelStepper::DRIVER, ARM0_M0_STEP, ARM0_M0_DIR);
+AccelStepper arm0_m0(AccelStepper::DRIVER, ARM0_M0_STEP, ARM0_M0_DIR);
+AccelStepper arm0_m0(AccelStepper::DRIVER, ARM0_M0_STEP, ARM0_M0_DIR);
+AccelStepper arm0_m0(AccelStepper::DRIVER, ARM0_M0_STEP, ARM0_M0_DIR);
+AccelStepper arm0_m0(AccelStepper::DRIVER, ARM0_M0_STEP, ARM0_M0_DIR);
 
-const double start_arm0_theta0 = 2*LENGTH*cos(PI/4);
-const double start_arm0_theta1 = 2*LENGTH*sin(PI/4);
-const double start_arm1_theta0 = 2*LENGTH*cos(3*PI/4);
-const double start_arm1_theta1 = 2*LENGTH*sin(3*PI/4);
-
-void init_pins()
-{
-  arm0.motor0_step_pin = 2;
-  arm0.motor1_step_pin = 3;
-  arm0.motor0_dir_pin = 4;
-  arm0.motor1_dir_pin = 5;
-
-  arm1.motor0_step_pin = 6;
-  arm1.motor1_step_pin = 7;
-  arm1.motor0_dir_pin = 8;
-  arm1.motor1_dir_pin = 9;
-}
+// all stepper motors controlled by one interface
+MultiStepper all_motors;
+long motor_positions[4] = {0.0, 0.0, 0.0, 0.0};
 
 void calibrate_motors()
 {
   pregame_arm_data *arm_data = read_arm_pregame();
-  arm0.arm_theta0 = arm_data->arm0_theta0;
+  // remember to take note that the arms for some reason may not be visible
+  // whatsoever, so also have a boolean in the struct that says whether
+  // the arm angles were all sucessfully detected, and if not have the
+  // arduino give some warning to users that calib unsuccessful
+  arm0_m0.setCurrentPosition(DEG_TO_STEP(arm_data->arm0_theta0));
+  motor_positions[0] = DEG_TO_STEP(arm_data->arm0_theta0);
+  ..........
   arm0.arm_theta1 = arm_data->arm0_theta1;
   arm1.arm_theta0 = arm_data->arm1_theta0;
   arm1.arm_theta1 = arm_data->arm1_theta1;
-  // free(pregame_arm_data);
-
-  // probably run these two in parallel
-  move_arm(arm0, start_arm0_theta0, start_arm0_theta1);
-  move_arm(arm1, start_arm1_theta0, start_arm1_theta1);
+  free(pregame_arm_data);
 }
 
-void move_arm(store_arm_data arm, float goal_theta0, float goal_theta1)
-{
-  float diff0 = goal_theta0 - arm.arm_theta0;
-  float diff1 = goal_theta1 - arm.arm_theta1;
+void calculate_steps(float x, float y) {
 
-  // replace all of this with a ros trajectory
-  while(diff0 > 1 && diff1 > 1) {
-    // to make stepper motors move one step, generate square wave signal
-    digitalWrite(arm.motor0_step_pin, HIGH);
-    digitalWrite(arm.motor1_step_pin, HIGH);
-    digitalWrite(arm.motor0_step_pin, HIGH);
-    digitalWrite(arm.motor1_step_pin, HIGH);
-    delayMicroseconds(300);
+  // ----- locals
+  float
+  ,             //pen distance to motors
+  angle0,               //motor1 angle
+  angle1;               //motor2 angle
 
-    digitalWrite(arm.motor0_step_pin, LOW);
-    digitalWrite(arm.motor1_step_pin, LOW);
-    digitalWrite(arm.motor0_step_pin, LOW);
-    digitalWrite(arm.motor1_step_pin, LOW);
-    delayMicroseconds(300);
-    arm.arm_theta0 += STEP_ROT;  // each step is a certain angle
-    arm.arm_theta1 += STEP_ROT;
+  // ----- calculate distances
+  // distance = sqrt((OFFSET - x) * (OFFSET - x) + (YAXIS - y) * (YAXIS - y));
 
-    // NOTE: need to convert normal stepper rotation to belt translation
-    // for second arm since motor drives a belt and armindirectly
-    diff0 = goal_theta0 - arm.arm_theta0;
-    diff1 = goal_theta1 - arm.arm_theta1;
-  }
+  // with 2 DOF, there are usually two different solutions to a position
+  // temporarily just default to one
+  // assum both arm links are same length, so 2*SQ(LENGTH)
+  angle1 = acos((SQ(x) + SQ(y) - 2*SQ(LENGTH))/2*SQ(LENGTH));
+  angle0 = atan(y/x) - atan((LENGTH*sin(angle1))/(LENGTH + LENGTH*cos(angle1)));
+
+  // ----- calculate steps required to reach (x,y) from 12 o'clock
+  STEPS0 = int(DEG_TO_STEP(RAD_TO_DEG(angle0)));
+  STEPS1 = int(DEG_TO_STEP(RAD_TO_DEG(angle1)));
+  return {STEPS0, STEPS1};
 }
-
 
 void setup() {
-  init_pins();
   // set motor control pin modes
-  pinMode(arm0.motor0_step_pin, OUTPUT);
+  pinMode(ARM0_M0_STEP, OUTPUT);
+  arm0_m0.setMaxSpeed(1000.0);  // default
+  arm0_m0.setAcceleration(100.0);  // default
+  ..........
   pinMode(arm0.motor1_step_pin, OUTPUT);
   pinMode(arm0.motor0_dir_pin, OUTPUT);
   pinMode(arm0.motor1_dir_pin, OUTPUT);
@@ -88,10 +89,15 @@ void setup() {
   pinMode(arm1.motor0_dir_pin, OUTPUT);
   pinMode(arm1.motor1_dir_pin, OUTPUT);
   // digitalWrite(dirPin,HIGH); //Enables the motor to move in a particular direction
+
+  all_motors.addStepper(arm0_m0);
+  .............
 }
 
 
 void loop() {
+  all_motors.moveTo(motor_positions);
+
 //  rec_arm_data *new_goal = read_arm_goals();
 //  move_arm(arm0, new_goal->arm_x, new_goal->arm_y);
 //  // move_arm(arm1, start_x1, start_y1);
